a8d9839f05a05e249e18178543c5097d59eae5a0
Mark Daquis <36392970+markzero27@users.noreply.github.com>
2021-01-07 23:41:33 +0800

    initialized sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mark-daquis-portfolio';
+  isNavToggled: boolean = false;
+
+  constructor(public router: Router) {}
+
+  toggleDrawer() {
+    this.isNavToggled = !this.isNavToggled;
+  }
 }
